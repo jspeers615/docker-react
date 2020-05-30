59303430da4b8fbf99ce28323cd4ea6bdbcd775f
Jordan Speers <jspeers615@live.co.uk>
2020-05-31 00:49:23 +0100

    Update react app

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <a
+        className="App-link"
+        href="https://lh3.googleusercontent.com/proxy/vYrri3Y-w_GnelRv--YTWvIrtXwCZVDzgXiYsXwgCDDOYX1HBWmpJ3WrrVsgLve9zNY1WquW6KV1tEWu-_HJRYrjp5fPiR6mvYpLHCdxfdRm7GB90AD8f3YaPogPzGc"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Yummy grapes
+      </a>
+      <br></br>
         <p>Quack Quack
         <img src="https://api.time.com/wp-content/uploads/2019/12/ducks-walk-on-glass-roof-video.jpg"></img>
         </p>
-        <a
-          className="App-link"
-          href="https://lh3.googleusercontent.com/proxy/vYrri3Y-w_GnelRv--YTWvIrtXwCZVDzgXiYsXwgCDDOYX1HBWmpJ3WrrVsgLve9zNY1WquW6KV1tEWu-_HJRYrjp5fPiR6mvYpLHCdxfdRm7GB90AD8f3YaPogPzGc"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Yummy grapes
-        </a>
+        <br></br>
       </header>
     </div>
   );
